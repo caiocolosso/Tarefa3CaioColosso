b370499c9286a14683a1bfc2c86a0350953248a9
caiocolosso <72581701+caiocolosso@users.noreply.github.com>
2020-10-10 21:25:50 -0300

    Create Tarefa3CaioColosso.py

---
+++ Tarefa3CaioColosso.py
@@ -0,0 +1,24 @@
+#Primeiro, é necessário entender qual a relação entre segundos e horas. Temos que 1 hora é igual a 60 minutos,
+# e cada minuto é igual a 60 segundos. Logo:
+# s=h*(60*60)
+
+#'1': Para uma hora e meia, h=1.5
+h=1.5
+s=h*(60*60)
+print('uma hora e meia é igual a', s, 'segundos')
+s=5400.00
+#Resposta exercício '1': Para h=1.5, S=5400 segundos.
+
+#'2': para 12 horas, temos que:
+h=12
+s=h*60*60
+print('doze horas é igual a', s, 'segundos')
+s=43200.00
+#Resposta exercício '2': Para h=12, S=43200 segundos.
+
+#'3': Para 24 horas, temos que:
+h=24
+s=h*60*60
+print('vinte e quatro horas é igual a', s, 'segundos')
+s=86400.00
+#Resposta exercício '3': Para h=12, S=86400 segundos.
